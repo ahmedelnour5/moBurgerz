e8c9cd69f690de57761b0f4e125bfa9e8f2fdf18
Ahmed Elnour <ahmedelnour@Ahmeds-Air.fios-router.home>
2023-11-12 13:22:31 -0500

    fix modal styles on desktop

--- frontend/src/Components/EditItemModal.jsx
+++ frontend/src/Components/EditItemModal.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Modal } from "@mui/base/Modal";
 
 const EditItemModal = ({ open, onClose, children }) => {
   if (!open) return null;
@@ -16,10 +15,10 @@ const EditItemModal = ({ open, onClose, children }) => {
   return (
     <div
       onClick={handleClose}
-      className="fixed inset-y-2/4 translate-y-2/4 md:inset-0  lg:backdrop-blur-md flex justify-center items-center"
+      className="fixed inset-y-2/4 translate-y-2/4 md:inset-0 md:translate-y-0 lg:backdrop-blur-sm flex justify-center items-center"
       id="wrapper"
     >
-      <div className=" w-[85%] md:w-[600px]">
+      <div className=" w-[85%] md:w-[400px] lg:w-[600px]">
         <div className=" bg-white p-3 rounded-md h-[500px] flex flex-col">
           <button
             className="text-red-500 text-xl place-self-end font-bold"
