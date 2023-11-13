bf10401da49efc0ca7ac9f2db4d6f6654eec637c
Ahmed Elnour <ahmedelnour@Ahmeds-Air.fios-router.home>
2023-11-12 22:15:58 -0500

    change vercel.json to include index.js path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,8 +7,8 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
+      "/api": {
+        target: "mo-burgerz-backend.vercel.app",
         changeOrigin: true,
       },
     },
