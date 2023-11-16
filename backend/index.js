5257a4012e666c201edcfc3b212d4c9536eb5a17
Ahmed Elnour <ahmedelnour@Ahmeds-Air.fios-router.home>
2023-11-16 16:15:08 -0500

    add production enviornment code to serve static frontend files

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/users", userRoutes);
 app.use("/menu", menuRoutes);
 app.use("/checkout", checkoutRoutes);
-app.get("/", (req, res) => res.json("Server is ready"));
+
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => res.send("Server is ready"));
+}
 
 app.use(notFound);
 app.use(errorHandler);
