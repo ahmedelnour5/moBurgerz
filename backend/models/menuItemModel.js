aa505681c1cf8a5163228fd1ab5345982eaf666e
Ahmed Elnour <ahmedelnour@Ahmeds-Air.fios-router.home>
2023-11-17 15:16:49 -0500

    fix Order page error preventing items from loading

--- backend/models/menuItemModel.js
+++ backend/models/menuItemModel.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
+import Ingredients from "./ingredientsModel.js";
 
 const menuItemSchema = mongoose.Schema({
   name: String,
   price: Number,
   category: String,
   description: String,
-  modifications: [{ type: mongoose.SchemaTypes.ObjectId, ref: "ingridients" }],
+  modifications: [{ type: mongoose.SchemaTypes.ObjectId, ref: "Ingridients" }],
 });
 
 const MenuItem = mongoose.model("MenuItem", menuItemSchema);
